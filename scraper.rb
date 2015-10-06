db1e02b4ab04504cf46f778beb85e9fee88e0229
Tony Bowden <tony@tmtm.com>
2015-10-06 15:39:23 +0100

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,66 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'colorize'
+require 'json'
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+require 'scraperwiki'
+require 'yaml'
+
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+
+def json_from(url)
+  JSON.parse(open(url).read, symbolize_names: true)
+end
+
+def yaml_from(url)
+  YAML.parse(open(url).read).to_ruby
+end
+
+def scrape_members(term, url)
+  json_from(url).each do |file|
+    mp = yaml_from(file[:download_url])
+    next if mp['tenures'].nil?
+
+    data = { 
+      id: nil,
+      name: mp['name']['en'],
+      name__en: mp['name']['en'],
+      name__el: mp['name']['el'],
+      email: mp['email'],
+      image: mp['mugshot'],
+      wikipedia: mp['links'].map{ |l| l['url'] }.find { |l| l.include? 'wikipedia' },
+      source: mp['links'].map{ |l| l['url'] }.find { |l| l.include? 'parliament.cy' },
+    }
+    data[:id] = data[:identifier__parliament_cy] = File.basename data[:source]
+    data[:identifier__openpatata] = File.basename(url, '.*')
+    mp['tenures'].select { |tenure|
+      tenure['end_date'].to_s.empty? || tenure['end_date'].to_s >= term[:start_date]
+    }.each do |tenure|
+      tenure['parl_group'] ||= { 'en' => 'Independent', 'el' => 'Independent' }
+      mem = data.merge({ 
+        term: term[:id],
+        start_date: tenure['start_date'],
+        end_date: tenure['end_date'],
+        area: tenure['district']['en'],
+        area__el: tenure['district']['el'],
+        faction: tenure['parl_group']['en'],
+        faction__el: tenure['parl_group']['el'],
+      })
+      ScraperWiki.save_sqlite([:id, :term, :faction, :start_date], mem)
+    end
+  end
+end
+
+
+term = {
+  id: 2011,
+  name: '2011–',
+  start_date: '2011-06-02',
+}
+ScraperWiki.save_sqlite([:id], term, 'terms')
+
+scrape_members(term, 'https://api.github.com/repos/openpatata/openpatata-data/contents/mps')
